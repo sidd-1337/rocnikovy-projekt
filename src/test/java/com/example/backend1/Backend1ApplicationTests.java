f14e86746045bb711beb3ad6dc69f0fc8ce06430
Nela-B <nbulavova@gmail.com>
2024-02-04 23:38:53 +0100

    Enable to run all the tests from one script.

--- src/test/java/com/example/backend1/Backend1ApplicationTests.java
+++ src/test/java/com/example/backend1/Backend1ApplicationTests.java
@@ -3,6 +3,24 @@ package com.example.backend1;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+//import org.junit.platform.suite.api.SuiteExecutionMode;
+import com.example.backend1.controller.OborIdIntegrationTest;
+import com.example.backend1.controller.ProgrammeIntegrationTest;
+import com.example.backend1.controller.SubjectIntegrationTest;
+import com.example.backend1.repository.DuplicityTest;
+
+
+
+@Suite
+//@SuiteExecutionMode(SuiteExecutionMode.CONCURRENT) // Optional: run tests concurrently
+@SelectClasses({
+		OborIdIntegrationTest.class,
+		ProgrammeIntegrationTest.class,
+		SubjectIntegrationTest.class,
+		DuplicityTest.class // Assuming DuplicityTest contains both duplicity tests you mentioned
+})
 @SpringBootTest
 class Backend1ApplicationTests {
 
