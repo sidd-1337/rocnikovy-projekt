fc9d6972e2e5d1b8164f54fb4ed676568d95a417
sidd-1337 <78906039+sidd-1337@users.noreply.github.com>
2024-05-12 17:34:04 +0200

    CommandLineRunner merged to one command instead of 3

--- src/main/java/com/example/backend1/Backend1Application.java
+++ src/main/java/com/example/backend1/Backend1Application.java
@@ -15,33 +15,23 @@ public class Backend1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Backend1Application.class, args);
 	}
-	//@Bean
-	//public CommandLineRunner run(ProgrammesService programmeService) {
-	//	return args -> {
-	//		// Save data for each faculty
-	//		programmeService.fetchDataAndSave("FAU");
-	//		programmeService.fetchDataAndSave("FFI");
-	//		programmeService.fetchDataAndSave("FPD");
-	//		programmeService.fetchDataAndSave("FPR");
-	//		programmeService.fetchDataAndSave("FSS");
-	//		programmeService.fetchDataAndSave("FZS");
-	//	};
-	//}
-    /*@Bean
-    public CommandLineRunner run(TempSubjectsService tempSubjectsService) {
-        return args -> {
-            // Call fetchDataAndSave with the desired parameters
-            tempSubjectsService.fetchDataAndSave("KSN", "1VLA5");
-        };
-    //}
+	@Bean
+	public CommandLineRunner run(ProgrammesService programmeService, TempSubjectsService tempSubjectsService, SubjectsService subjectsService) {
+		return args -> {
+			// Save data for each faculty in ProgrammesService
+			programmeService.fetchDataAndSave("FAU");
+			programmeService.fetchDataAndSave("FFI");
+			programmeService.fetchDataAndSave("FPD");
+			programmeService.fetchDataAndSave("FPR");
+			programmeService.fetchDataAndSave("FSS");
+			programmeService.fetchDataAndSave("FZS");
 
+			// Save data for TempSubjectsService
+			tempSubjectsService.fetchDataAndSave("KSN", "1VLA5");
 
-    @Bean
-    public CommandLineRunner run(SubjectsService subjectsService) {
-        return args -> {
-            // Call fetchDataAndSave with the desired parameters
-            subjectsService.fetchDataAndSave("3071", "LS");
-        };
-    */
+			// Save data for SubjectsService
+			subjectsService.fetchDataAndSave("3071", "LS");
+		};
+	}
 
 }
